ca11363f9d21336f74fc651d7cc9c4b05689ebbc
Madina <madinatussupova0820@gmail.com>
2022-07-21 15:55:10 +0600

    calculate and display summary

--- script.js
+++ script.js
@@ -61,15 +61,17 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function(movements) {
+const displayMovements = function (movements) {
   containerMovements.innerHTML = '';
 
-  movements.forEach(function(mov, i) {
+  movements.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
     <div class="movements__row">
-      <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+      <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
       <div class="movements__value">${mov}</div>
     </div>
     `;
@@ -81,24 +83,50 @@ displayMovements(account1.movements);
 
 // Calculating and Displaying Balance
 
-const calcDisplayBalance = function(movements) {
+const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} EUR`
-}
+  labelBalance.textContent = `${balance}€`;
+};
 calcDisplayBalance(account1.movements);
+
+//  Calculating and Displaying Summary
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+calcDisplaySummary(account1.movements);
 
 // Computing Usernames
 
-const createUsernames = function(accs) {
-  accs.forEach(function(acc) {
-    acc.username = acc.owner.toLowerCase().split(' ').map(name => name[0]).join('')
-  })
-}
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
 createUsernames(accounts);
 console.log(accounts);
-
-
-
 
 ////////////////////////////// LECTURES
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
@@ -128,15 +156,26 @@ console.log(balance);
 // }, 0);
 // console.log(balance);
 
-
-
 let balance2 = 0;
-for(const mov of movements) balance2 += mov;
+for (const mov of movements) balance2 += mov;
 console.log(balance2);
 
 // Maximum value
 const max = movements.reduce((acc, mov) => {
-  if(acc > mov) return acc;
+  if (acc > mov) return acc;
   else return mov;
 }, movements[0]);
 console.log(max);
+
+// Chaining Methods
+const eurotoUsd = 1.1;
+console.log(movements);
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  // .map((mov, i, arr) => {
+  //   console.log(arr);
+  //   return mov * eurotoUsd;
+  // })
+  .map(mov => mov * eurotoUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
