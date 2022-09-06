cdcdc8450166c58b233b72f6a27b7e6ed37c34b6
Madina <madinatussupova0820@gmail.com>
2022-09-06 09:55:29 +0600

    adding dates

--- script.js
+++ script.js
@@ -17,9 +17,9 @@ const account1 = {
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2022-09-01T14:43:26.374Z',
+    '2022-09-04T18:49:59.371Z',
+    '2022-09-06T12:01:20.894Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -37,9 +37,9 @@ const account2 = {
     '2019-12-25T06:04:23.907Z',
     '2020-01-25T14:18:46.235Z',
     '2020-02-05T16:33:06.386Z',
-    '2020-04-10T14:43:26.374Z',
-    '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
+    '2022-09-01T14:43:26.374Z',
+    '2022-09-05T18:49:59.371Z',
+    '2022-09-06T12:01:20.894Z',
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -73,6 +73,23 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+// Functions
+const formatMovementDate = function(date) {
+  const calcDaysPassed = (date1, date2) => Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+
+  if(daysPassed === 0) return 'Today';
+  if(daysPassed === 1) return 'Yesterday';
+  if(daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+    const day = `${date.getDate()}`.padStart(2, '0');
+    const month = `${date.getMonth() + 1}`.padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
+}
+
 // Displaying movements
 // Option of sorting movements in ascending order
 
@@ -87,20 +104,15 @@ const displayMovements = function (acc, sort = false) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const date = new Date(acc.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, '0');
-    const month = `${date.getMonth() + 1}`.padStart(2, '0');
-    const year = date.getFullYear();
+    const displayDate = formatMovementDate(date);
 
-    const displayDate = `${day}/${month}/${year}`;
-    const html = `
-    <div class="movements__row">
+    const html = `<div class="movements__row">
       <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
       <div class="movements__date">${displayDate}</div>
       <div class="movements__value">${mov.toFixed(2)}</div>
-    </div>
-    `;
+    </div>`;
 
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
@@ -185,8 +197,8 @@ btnLogin.addEventListener('click', function (event) {
     const day = `${now.getDate()}`.padStart(2, '0');
     const month = `${now.getMonth() + 1}`.padStart(2, '0');
     const year = now.getFullYear();
-    const hour = now.getHours().padStart(2, 0);
-    const min = now.getMinutes().padStart(2, 0);
+    const hour = now.getHours();
+    const min = now.getMinutes();
 
     labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
 
@@ -283,46 +295,17 @@ btnSort.addEventListener('click', function (e) {
 });
 
 // Fake always logged in
-currentAccount = account1;
-updateUI(currentAccount);
-containerApp.style.opacity = 100;
+// currentAccount = account1;
+// updateUI(currentAccount);
+// containerApp.style.opacity = 100;
 
 ////////////////////////////// LECTURES
 
-// Create a date
-/*
-const now = new Date();
-console.log(now);
+// const future = new Date(2023, 10, 27, 10, 45);
 
-console.log(new Date('December 25, 2022'));
+// console.log(Number(+future));
 
-console.log(new Date(account1.movementsDates[0]));
+// // const calcDaysPassed = (date1, date2) => Math.abs(date2 - date1) / (1000 * 60 * 60 * 24); // times in milliseconds
 
-console.log(new Date(2023, 10, 27, 10, 45, 50)); // Mon Nov 27 2023 10:45:50 GMT+0600 (Восточный Казахстан)
-
-console.log(new Date(0)); // Thu Jan 01 1970 06:00:00 GMT+0600 (Восточный Казахстан)
-
-// Convert 3 days to milliseconds
-console.log(new Date(3 * 24 * 60 * 60 * 1000));
-
-// Working with dates
-const future = new Date(2023, 10, 27, 10, 45);
-console.log(future);
-console.log(future.getFullYear());
-console.log(future.getMonth()); // zerobased
-console.log(future.getDate()); // a day
-console.log(future.getDay()); // a weekday
-console.log(future.getHours());
-console.log(future.getMinutes());
-console.log(future.getSeconds());
-
-console.log(future.toISOString());
-console.log(future.getTime()); // 1701060300000 milliseconds
-
-console.log(new Date(1701060300000)); // Mon Nov 27 2023 10:45:00 GMT+0600 (Восточный Казахстан)
-
-console.log(Date.now()); // 1661931897741 returns time in milliseconds
-
-future.setFullYear(2022);
-console.log(future);
-*/
+// const days1 = calcDaysPassed(new Date(2022, 9, 21), new Date(2023, 10, 25)); // 10
+// console.log(days1);
